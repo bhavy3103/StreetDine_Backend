f7b5a4f96f0c50accf2e66fce34ca5e3b8fa81be
Bhavy <bhavypdevani31@gmail.com>
2024-01-29 19:35:58 +0530

    connected to database and create demo test api

--- server/index.js
+++ server/index.js
@@ -1,6 +1,22 @@
-import express from 'express';
+import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+import cors from "cors";
+dotenv.config();
+import userRoutes from './routes/userRoutes.js';
+
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log('Connected to MongoDB🥳🥳🚀');
+}).catch((err) => {
+  console.log(err);
+});
 
 const app = express();
+app.use(cors());
+
+
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log('Server is running on port 3000');
 })
+
+app.use('/api/user', userRoutes);
